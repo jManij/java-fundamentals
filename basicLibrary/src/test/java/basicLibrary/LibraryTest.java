d35fe85852fdcdc62b6c124938d7efafe139dcb8
Manish KC <kcmanish89@gmail.com>
2019-08-07 14:29:38 -0700

    lab-02 complete

--- basicLibrary/src/test/java/basicLibrary/LibraryTest.java
+++ basicLibrary/src/test/java/basicLibrary/LibraryTest.java
@@ -7,29 +7,36 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-//    @Test public void testSomeLibraryMethod() {
-//        Library classUnderTest = new Library();
-//        assertTrue("someLibraryMethod should return 'true'", classUnderTest.roll());
-//    }
-
-
     @Test
     public void roll() {
-        Library classUndertest = new Library();
-//        assertArrayEquals("", classUndertest.roll(5));
-//        assertArrayEquals(int[], classUndertest.roll(5));
+        Library classUnderTest = new Library();
+        assertEquals(5, classUnderTest.roll(5).length);
+        assertEquals(6, classUnderTest.roll(6).length);
 
     }
 
     @Test
     public void containsDuplicates() {
+        Library classUnderTest = new Library();
+        assertTrue("Should return true",  classUnderTest.containsDuplicates(new int[]{1,2,3,4,5,6,1}));
+        assertFalse("Should return false", classUnderTest.containsDuplicates(new int[]{1,2,3,4,5,6,7}));
+
     }
 
     @Test
     public void calculateAverages() {
+        Library classUnderTest = new Library();
+        assertEquals("The output expected",3, classUnderTest.calculateAverages(new int[]{1,2,3,4, 5}));
     }
 
     @Test
     public void averageOfArraysOfArrays() {
+        Library classUnderTest = new Library();
+        assertEquals("Received wrong output",57,classUnderTest.averageOfArraysOfArrays(new int[][]{
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        }));
     }
 }
